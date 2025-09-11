ea384124daede156dd4aaaf3533c37f4032f7888
Uladzimri Klyshevich <uladzimir_klyshevich@epam.com>
2025-09-11 18:05:25 +0300

    added zip artefacts after deployment for downloading

---
+++ lib/core/config/runtime_config_stub.dart
@@ -0,0 +1,8 @@
+// Stub implementation for non-web platforms
+// This file is used when dart:js is not available (mobile, desktop, tests)
+
+/// Get JavaScript configuration - stub implementation
+/// Returns null on non-web platforms
+dynamic getJsConfig() {
+  return null;
+}
